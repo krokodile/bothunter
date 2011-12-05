cd5f86493b3755537a7ea8e38d7c202074e6685c
Boris Kraportov <boris@iloveboris.com>
2011-12-05 18:30:26 +0400

    fixed enciroments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Telefront::Application.configure do
+Bothunter::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Telefront::Application.configure do
+Bothunter::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
