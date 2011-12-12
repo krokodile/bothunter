5f4460a6592f639646efe0c48ccd6eefe0ccfc31
Boris Kraportov <boris@iloveboris.com>
2011-12-12 20:25:53 +0400

    Fixed controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,7 @@ class PagesController < ApplicationController
     #group.save
     group.users << current_user
     group.save
+    Resque.enqueue(Vk::GroupUsersParse, group.gid)
     redirect_to :action=>'index'
   end
 
