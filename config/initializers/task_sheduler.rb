d084e1de48a9e668620339a280e76a8e4aa56b45
Boris Kraportov <boris@iloveboris.com>
2011-12-14 14:57:34 +0400

    capistrano settings

--- config/initializers/task_sheduler.rb
+++ config/initializers/task_sheduler.rb
@@ -1,6 +1,12 @@
 scheduler = Rufus::Scheduler.start_new  
   
-scheduler.every("12h") do
-   ParseGroups.perform
-   ParseUsers.perform
+scheduler.every("2m") do
+   ::Group.all.each do |group|
+      puts "detect group: #{group.title}"
+      Vk::GroupUsersParse.perform(group.gid || group.domain  || group.link)
+    end
+
+   ::Person.where(state: :pending).all.each do |person|
+       Vk::ProfileParse.perform(person)
+    end
 end 
