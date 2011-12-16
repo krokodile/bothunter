726c6905fefcdf6f9ec24f8a912a1a90e096a1bc
Boris Kraportov <boris@iloveboris.com>
2011-12-16 18:13:02 +0400

    added bot remover

--- app/workers/vk/group/users/bot_filter.rb
+++ app/workers/vk/group/users/bot_filter.rb
@@ -1,45 +1,68 @@
 class BotFilter
   def self.delete_robots (gid, login, password)
+    #gid - group id, string
     cookies = VkontakteHeaders.user_sign_in(login, password)
     group = ::Vkontakte.find_group gid
     puts cookies
     puts "detecting users of #{group.title}"
-    gid = group.gid
+    #gid = group.gid
     ::Vkontakte.parse_each_item({
       method: 'post',
-      offset: 96,
-      url: 'al_page.php',
+      gjson: false,
+      offset: 25,
+      url: 'al_groups.php',
+      cookies: cookies,
       thread_count: THREAD_COUNT,
       params: {
-        act: 'show_members_box',
+        act: 'people_get',
         al: 1,
         tab: 'members',
-        gid: gid,
-        cookies: cookies
+        gid: gid
       },
-      item_for_parse: 'div.wide_box_user',
+      item_for_parse: '.group_p_row',
     }) do |persons|
       #TODO: Remove sign out users
-
+      puts "Size is: #{persons.size}"
       persons.each do |person_source|
+        #puts "Iterate!"
+        #puts person_source
         person_html = person_source.to_nokogiri_html
-        person_link = Vk::arg2uid (Nokogiri::HTML(person_source) / 'a:first').first['href']
-        if person_link.present?
-          puts person_link
-          #if Vk.uid? person_link
-          person = ::Vkontakte.find_person(person_link)
-          if !group.persons.include?(person)
-             js = (person_html / 'div.group_p_actions').first.content
-             puts js
+        #puts person_html
+        #person = ::Vkontakte.find_person(person_link)
+        #if !group.persons.include?(person)
+        js = (person_html / '.group_p_actions a').first[:onclick]
+        js_match = /GroupsEdit.memberAction\(this, 'delete', (\d*), (\d*), '(.*)', -1\)/.match(js)
+        uid = js_match[2]
+        hash = js_match[3]
+        #puts "UID: #{uid} HASH: #{hash}"
+        if Person.where(uid: uid, state: :robot).count>0
+          puts "DELETE #{uid}"
+          req_params = {
+              cookies: cookies,
+              act: 'member_action',
+              action: 'delete',
+              al: '1',
+              gid: gid,
+              hash: hash,
+              mid: uid
+          }
+          begin
+            #TODO make normal code, please
+            Vkontakte.http_post("/al_groups.php",req_params)
+            Kernel.sleep(1)
+          rescue => e
+            puts "Raised error on #{uid}, analitycs nead"
           end
-          group.save
+          Person.where(uid: uid, state: :robot).delete
+        end
+        #  end
+          #group.save
           #::ProfileParse.perform person
 
           #else
           #group.people << Vk::Person.find_or_create_by(domain:person_link)
           #end
-        end
-
+        #end
       end
     end
   end
