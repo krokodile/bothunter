1ae0e363767b03d4e09a0efba259d433780687eb
Boris Kraportov <boris@iloveboris.com>
2012-01-11 16:38:14 +0400

    little serializer fix

--- app/workers/vk/profile/all_profiles_parse.rb
+++ app/workers/vk/profile/all_profiles_parse.rb
@@ -3,14 +3,14 @@ module Vk::AllProfilesParse
     persons = []
     api = ::Vk::API.new(::AccountStore.next(:vkontakte, :accounts)['token'])
 
-    Person.all.each do  |person|
+    Person.limit(10).all.to_a.each do  |person|
       puts person.uid || person.domain
       begin
         profile = api.getProfiles({
           uids: person.uid || person.domain,
           fields: 'uid, first_name, last_name, nickname, screen_name, sex, bdate, city, country, timezone, photo, photo_medium, photo_big, has_mobile, rate, contacts, education, online, counters'
         })
-        persons << profile
+        persons << profile[0]
       rescue
         puts "user #{person.uid || person.domain} fails"
       end
