f5ec1a924a08b8ecf22ebd5951605a2a7752c0a7
Lain Iwakura <lain@navi.(none)>
2012-01-25 18:19:27 +0400

    mechanize monkeypatch fix

--- config/initializers/socksify_http_agents.rb
+++ config/initializers/socksify_http_agents.rb
@@ -4,10 +4,14 @@ public
   def set_socks addr, port
     set_http unless @http
     class << @http
+      attr_accessor :socks_addr, :socks_port
+
       def http_class
-        Net::HTTP.SOCKSProxy(addr, port)
+        Net::HTTP.SOCKSProxy(socks_addr, socks_port)
       end
     end
+    @http.socks_addr = addr
+    @http.socks_port = port
   end
 end
 
