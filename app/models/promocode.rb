9041e60e2d155b47327b3aacd62b1ddf6658e773
Lain Iwakura <lain@navi.(none)>
2012-03-11 12:56:53 +0400

    promocode#code property

--- app/models/promocode.rb
+++ app/models/promocode.rb
@@ -1,29 +1,32 @@
 class Promocode
   include Mongoid::Document
 
-  before_create :multiply
+  #before_create :multiply
 
   field :groups_limit, type: Integer, default: 0
   field :people_limit, type: Integer, default: 0
 
-  attr_accessor :promocodes_count # reader needed for form generation
+  #attr_accessor :promocodes_count # reader needed for form generation
 
   has_one :user
 
-protected
-  def multiply
-    count = @promocodes_count.to_i
-    
-    if count > 1
-      count = count.pred
+  field :code, type: String, default: nil
+  index :code
 
-      attrs = self.attributes
-      attrs.delete :_id
-      attrs.delete '_id'
-      attrs.delete :id
-      attrs.delete 'id'
-
-      count.times{ Promocode.create attrs }
-    end
-  end
+#protected
+#  def multiply
+#    count = @promocodes_count.to_i
+#    
+#    if count > 1
+#      count = count.pred
+#
+#      attrs = self.attributes
+#      attrs.delete :_id
+#      attrs.delete '_id'
+#      attrs.delete :id
+#      attrs.delete 'id'
+#
+#      count.times{ Promocode.create attrs }
+#    end
+#  end
 end
