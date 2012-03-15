ea7bcfb10931a2beee1e32dea3decfe98426f82e
Boris A. Kraportov <boris@boris-K52Dr.(none)>
2012-03-15 11:35:59 +0400

    fix parse_users cursor lags

--- app/workers/vk/group/users/parse_users.rb
+++ app/workers/vk/group/users/parse_users.rb
@@ -5,22 +5,19 @@ class ParseUsers
     persons = ::Person.where(state: :pending).all
     threads_count = PARSE_USERS_THREADS
     threads = []
-    Group.all.each do |group|
-      persons = group.persons.where(state: :pending).to_a
-      #threads = threads.find_all {|thr| thr["state"]==:work}
-      persons.each do |person|
-        if threads.size>=threads_count
-          threads.each {|thr| thr.join }
-          threads = []
+    persons = Person.where(state: :pending).to_a
+    persons.each do |person|
+      if threads.size>=threads_count
+        threads.each {|thr| thr.join }
+        threads = []
+      end
+      threads << Thread.new do
+        begin
+          Vk::ProfileParse.perform(person)
+        rescue Exception=>e
+          puts e
         end
-        threads << Thread.new do
-          begin
-            Vk::ProfileParse.perform(person)
-          rescue Exception=>e
-            puts e
-          end
-        end
-     end
+      end
     end
   end
 end
