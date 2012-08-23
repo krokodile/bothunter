f3800fa994a158599e58350f398614bd224d7966
Timothy Klim <klimtimothy@gmail.com>
2012-08-23 16:19:23 +0400

    update deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ server '89.104.91.109', :web, :app
 #server domain, :db, :primary => true
 
 set :port, 2122
-set :branch, "master"
+set :branch, "develop"
 set :rails_env, "production"
 set :environment, "production"
